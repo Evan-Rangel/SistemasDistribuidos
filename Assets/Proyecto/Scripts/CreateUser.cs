de992c04d4de171ad8434625c461d69e9ad3b2be
Evan-Rangel <evan.rangel@uniat.edu.mx>
2023-05-17 18:08:33 -0600

    conexion

--- Assets/Proyecto/Scripts/CreateUser.cs
+++ Assets/Proyecto/Scripts/CreateUser.cs
@@ -16,7 +16,7 @@ public class CreateUser : MonoBehaviour
     [SerializeField] string nombre, apellidos, correo, password, nickname, date;
     [SerializeField] int tel;
 
-    string _URL = "C:/wamp64/www/LastFantasy/conn.php";
+    string _URL = "http://localhost/LastFantasy/conn.php";
 
     IEnumerator CrearUsuario(string _date, string _nombre, string _apellidos, string _correo, string _password, string _nickname, int _tel)
     {
@@ -31,9 +31,45 @@ public class CreateUser : MonoBehaviour
         form.AddField("nickname",_nickname);
 
 
-        UnityWebRequest www = UnityWebRequest.Post(_URL, form);
+        using (UnityWebRequest www = UnityWebRequest.Post(_URL, form))
+        {
+            yield return www.SendWebRequest();
 
-        yield return www.SendWebRequest();
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
 
     }
 
@@ -41,7 +77,11 @@ public class CreateUser : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            CrearUsuario(date,nombre,apellidos,correo,password,nickname,tel);
+            Debug.Log("Entro");
+
+            StartCoroutine(CrearUsuario(date, nombre, apellidos, correo, password, nickname, tel));
+            //StartCoroutine(GetRequest(_URL));
+        
         }
     }
 
